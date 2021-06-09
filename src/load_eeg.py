498e4cfd46b8163a459c8d9e44c48dca413e3aa5
josiah-d <duhaimejosiah@gmail.com>
2021-06-09 14:55:04 -0700

    plotting, refactor subj class

--- src/load_eeg.py
+++ src/load_eeg.py
@@ -1,42 +1,49 @@
-# import nme
-# import pandas as pd
+import mne
+import pandas as pd
 
 
-class LoadSubj:
+class Subj:
     """
     Loads a subject's EEG data.
     """
 
-    def __init__(self, subj_id, trial_id=None):
+    def __init__(self, subj_id):
         self.subj_id = subj_id
+        self.subj_filepath = f'data/ds002721-download/{subj_id}'
+        self.trial_filepath = None
+        self.data = None
+        self.data_df = None
+        self.channel_meta = None
+        self.eeg_meta = None
+        self.events = None
+        self.events_meta = None
+        self.epochs = None
+        
+    def add_trialpath(self, trial_id):
         self.trial_id = trial_id
-
-        #self.subj_filepath = f'data/ds002721-download/{subj_id}'
-        #self.trial_filepath = f'{self.subj_filepath}/eeg/{self.subj_id}_{self.trial_id}_eeg.edf'
-    
+        self.trial_filepath = f'{self.subj_filepath}/eeg/{self.subj_id}_{self.trial_id}'
+        
     def read_edf(self):
-        pass
+        self.data =  mne.io.read_raw_edf(f'../{self.trial_filepath}_eeg.edf', preload=True)
+        
+    def read_channels(self):
+        self.channel_meta = pd.read_csv(f'../{self.trial_filepath}_channels.tsv', delimiter='\t')
+    
+    def read_eeg_meta(self):
+        self.eeg_meta = pd.read_json(f'../{self.trial_filepath}_eeg.json', typ='series')
 
+    def read_events(self):
+        self.events = pd.read_csv(f'../{self.trial_filepath}_events.tsv', delimiter='\t')
+    
+    def read_events_meta(self):
+        self.events_meta = pd.read_json(f'../{self.trial_filepath}_events.json').T
 
-#raw = mne.io.read_raw_edf('data/ds002721-download/sub-01/eeg/sub-01_task-run2_eeg.edf', preload=True)
-
-
-subj_1 = LoadSubj(subj_id='sub-01', trial_id='task-run2')
-
-
-
-
-
-
-
-
-
-
-
-
-
-subj_id = 1234
-
-print('folder/folder/{}'.format(subj_id))
-print(f'folder/folder/{subj_id}')
-
+    def edf_dataframe(self):
+        channels = self.data.ch_names
+        data = self.data.get_data()
+        df = pd.DataFrame(data).T
+        df.columns = channels
+        self.data_df = df
+    
+    def define_epoch(self):
+        self.epochs = self.events[self.events['trial_type'] == 788]
--- src/plot_eeg.py
+++ src/plot_eeg.py
@@ -1,6 +1,34 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 10
+plt.rcParams['axes.labelsize'] = 10
+plt.rcParams['axes.titlesize'] = 10
+plt.rcParams['xtick.labelsize'] = 8
+plt.rcParams['ytick.labelsize'] = 8
+plt.rcParams['legend.fontsize'] = 10
+plt.rcParams['figure.titlesize'] = 12
 
+def plot_epoch(ax, start, stop, label='_nolegend_'):
+    return ax.axvspan(start, stop, facecolor='#990000', alpha=0.1, label=label)
 
-print(dir(pd))
+def all_leads(subj):
+    fig, ax = plt.subplots(19, sharex='all', figsize=(16, 9))
+    fig.suptitle(f'EEG Data\nSubject: {subj.subj_id}\nTrial: {subj.trial_id}')
+    
+    for i, col in enumerate(subj.data_df.columns):
+        if i == 0:
+            plot_epoch(ax[i], 75000, 80000, label='music')
+        else:
+            plot_epoch(ax[i], 75000, 80000)
+        plot_epoch(ax[i], 175000, 200000)
+
+        ax[i].plot(subj.data_df[col], linewidth=0.3)
+        ax[i].set_ylabel(col)
+        ax[i].set_yticks([])
+    
+    ax[-1].set_xlabel('Time (ms)')
+
+    fig.legend(loc='center right')
+    plt.savefig('../img/delme.jpg')
